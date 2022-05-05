51ce22fce8be66f58a2cf9c70fa225f1d408adfd
thakur-sahab999 <rt160918@gmail.com>
2022-05-05 09:19:30 +0530

    ready

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getGoals, setGoals, updateGoals, deleteGoals } = require('../controllers/goalController')
+const { getGoals, setGoals, updateGoals, deleteGoals } = require('../controllers/goalController.js')
 const {protect} = require('../middleware/authMiddleware')
 router.route('/').get(protect, getGoals).post(protect, setGoals)
 router.route('/:id').delete(protect, deleteGoals).put(protect, updateGoals)
